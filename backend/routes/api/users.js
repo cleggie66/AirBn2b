9b39404d2ad2ad34c75c3f10ff10c301b49b128c
cleggie66 <caleb@cleghorn.org>
2023-02-16 16:46:40 -0600

    signup complete and tested

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,19 @@
 const express = require('express');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
 
+router.post('/', async (req, res) => {
+    const { email, password, username } = req.body;
+    const newUser = await User.signup({ email, password, username })
+
+    await setTokenCookie(res, newUser);
+
+    return res.json({ user: newUser })
+})
 
 
 
