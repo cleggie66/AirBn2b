138ac420121e1764cb36069fb925c9930a2e7713
cleggie66 <caleb@cleghorn.org>
2023-03-13 12:55:42 -0500

    phase 3 checkpoint 2

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,34 @@
-const ProfileButton = () => {
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+    const dispatch = useDispatch();
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    const ulClassName = "profile-dropdown";
+
     return (
-        <div style={{ color: "orange", fontSize: "100px" }}>
-            <i className="fas fa-user-circle"></i>
-        </div>
+        <>
+            <button>
+                <div style={{ color: "orange", fontSize: "40px" }}>
+                    <i className="fas fa-user-circle" />
+                </div>
+            </button>
+            <ul className="profile-dropdown">
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>
+        </>
     );
-};
+}
 
 export default ProfileButton;
