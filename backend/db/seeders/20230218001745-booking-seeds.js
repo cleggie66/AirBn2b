3fdca81155951b0a6435c0ad1e2dfc57ab388dc3
Caleb Cleghorn <caleb@cleghorn.org>
2023-05-31 10:55:18 -0500

    updated booking seeder date template

--- backend/db/seeders/20230218001745-booking-seeds.js
+++ backend/db/seeders/20230218001745-booking-seeds.js
@@ -14,38 +14,38 @@ module.exports = {
       {
         spotId: 1,
         userId: 3,
-        startDate: '2024-01-10',
-        endDate: '2024-01-15'
+        startDate: '01-10-2024',
+        endDate: '01-15-2024'
       },
       {
         spotId: 2,
         userId: 4,
-        startDate: '2024-06-10',
-        endDate: '2024-06-19'
+        startDate: '06-10-2024',
+        endDate: '06-19-2024'
       },
       {
         spotId: 3,
         userId: 5,
-        startDate: '2024-03-03',
-        endDate: '2024-03-10'
+        startDate: '03-03-2024',
+        endDate: '03-10-2024'
       },
       {
         spotId: 4,
         userId: 1,
-        startDate: '2024-09-10',
-        endDate: '2024-09-12'
+        startDate: '09-10-2024',
+        endDate: '09-12-2024'
       },
       {
         spotId: 5,
         userId: 2,
-        startDate: '2024-04-13',
-        endDate: '2024-04-18'
+        startDate: '04-13-2024',
+        endDate: '04-18-2024'
       },
       {
         spotId: 4,
         userId: 2,
-        startDate: '2024-06-13',
-        endDate: '2024-06-18'
+        startDate: '06-13-2024',
+        endDate: '06-18-2024'
       },
     ], {})
   },
