d539f58d1e2761ad241b5e461c6c2b3925b8b201
Caleb Cleghorn <caleb@cleghorn.org>
2023-07-20 21:43:36 -0500

    cleanup

--- frontend/src/components/DeleteSpotModal/index.js
+++ frontend/src/components/DeleteSpotModal/index.js
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { deleteSpot } from "../../store/spotReducer";
-import "./DeleteSpotModal.css"
+import "./DeleteSpotModal.css";
 
 const DeleteSpotModal = ({ spot }) => {
     const { closeModal } = useModal();
