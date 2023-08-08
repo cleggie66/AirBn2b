37c2c39104ddbd3588b257d8ee3531d72aabfeb0
Caleb Cleghorn <caleb@cleghorn.org>
2023-08-08 16:43:44 -0500

    cleanup

--- frontend/src/components/SpotForm/UpdateSpotForm.js
+++ frontend/src/components/SpotForm/UpdateSpotForm.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { setAllSpots } from "../../store/spotReducer";
-import SpotForm from './index'
+import SpotForm from './index';
 
 const UpdateSpotForm = () => {
     const dispatch = useDispatch();
@@ -12,16 +12,16 @@ const UpdateSpotForm = () => {
     }, [dispatch]);
 
     const {spotId} = useParams();
-    const spotState = useSelector(state => state.spots.allSpots)
+    const spotState = useSelector(state => state.spots.allSpots);
     const spot = spotState[spotId];
 
     if (spot && Object.values(spot).length < 1) {
-        return (<h2>Loading...</h2>)
-    }
+        return (<h2>Loading...</h2>);
+    };
 
     return (
         <SpotForm spot={spot} formType="Update Spot" />
-    )
-}
+    );
+};
 
 export default UpdateSpotForm;
